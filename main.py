5a7a9edc9e478eee42b77a8a85fd0b23f4202aed
lanemunds <lanemunds@gmail.com>
2023-04-08 13:43:34 -0600

    select one

---
+++ main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+with open('website.html') as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser')
+heading = soup.find(name='h1', id='name')
+print(heading)
+
+section_heading = soup.find(name='h3', class_='heading')
+print(section_heading)
+
+company_url = soup.select_one(selector='p a')
+print(company_url)
