a63be28730db48da92e2ac35ebb03f0cf384ff3b
lanemunds <lanemunds@gmail.com>
2023-04-12 14:24:26 -0600

    third.py

--- second.py
+++ second.py
@@ -14,9 +14,16 @@ print(soup.title)
 page_name = soup.find(name='span', class_='titleline')
 
 page_name_a = page_name.find_all(name='a')
+article_links = []
+article_texts = []
 for article_tag in page_name_a:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get('href')
+    article_links.append(link)
 
-
-article_link = page_name_a.get('href')
 
 article_upvote = soup.find_all(name='span', class_='score').get_text()
+
+
+print(article_texts)
---
+++ third.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get('https://cougarboard.com')
+
+cougar_site = response.text
+soup = BeautifulSoup(cougar_site, 'html.parser')
+
+top_ten = soup.find(name='div', id='home-board')
+topTen = top_ten.find(name='cb-tab')
+list_box = topTen.find(name='div', class_='list_box')
+
+top = top_ten.find(name='h4')
+list_text = list_box.text
+
+head = soup.select_one(selector='div h4')
+print(list_text)
